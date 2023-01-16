dd7a7ddb7534a60bef456ef75695aaf7fc4fc1bb
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-17 00:10:34 +0530

    Basic Structure Implemented

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import words from "./words";
+const search = document.getElementById("searchBox");
+const listHolder = document.querySelector(".team");
 
-console.log(words);
+const getMeaning = async (word) => {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+search.addEventListener("keyup", async () => {
+  const val = search.value;
+  const results = words.filter((word) => word.includes(val)).splice(0, 5);
+  console.log(results);
+
+  const result = await getMeaning("hello");
+  console.log(result);
+});
